b55eb919a2bf8f375db80a689c6a3900fb23dca6
DucHuyVu03 <duchuyvu03.work@gmail.com>
2024-04-30 13:58:51 +0700

    test

--- OLED.ino
+++ OLED.ino
@@ -24,9 +24,9 @@
  included in any redistribution.
  **************************************************************************/
 
-#include <SPI.h>
+#include <SPI.h> 
 #include <Wire.h>
-#include <Adafruit_GFX.h>
+#include <Adafruit_GFX.h> 
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
